c795aae95517b288a70460abfe3ba769966b1c4a
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2018-12-04 19:33:33 -0500

    Update pretrained_glove.py

--- pretrained_glove.py
+++ pretrained_glove.py
@@ -13,3 +13,45 @@ def dist2(a, b):
   return 1 - a.dot(b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
+## more intuitive
+```
+def find_analogies(w1, w2, w3):
+  for w in (w1, w2, w3):
+    if w not in word2vec:
+      print("%s not in dictionary" % w)
+      return
+  king = word2vec[w1]
+  man = word2vec[w2]
+  woman = word2vec[w3]
+  v0 = king - main + woman
+  
+  min_dist = float('inf')
+  best_word = ''
+  for word, v1 in iteritmes(word2vec):
+    if word not in (w1, w2, w3):
+      d = dist(v0, v1)
+      if d < min_dist:
+        min_dist = d;
+        best_word = word;
+  print(w1, '-', w2, '=', best_word, '-', w3)
+```
+
+## faster
+def find_analogies(w1, w2, w3):
+    for w in (w1, w2, w3):
+    if w not in word2vec:
+      print("%s not in dictionary" % w)
+      return
+    king = word2vec[w1]
+    man = word2vec[w2]
+    woman = word2vec[w3]
+    v0 = king - main + woman
+    
+    ## What?
+    distances = pairwise_distances(v0.reshape(1, D), embedding, metric=metic).reshape(V)
+    idx = distances.argmin()
+    best_word = idx2word[idx]
+    
+    print(w1, '-', w2, '=', best_word, '-', w3)
+    
+
