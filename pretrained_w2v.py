c84619cc777f81723953b04c897ca0ec4fba86b9
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2018-12-04 19:56:19 -0500

    Update pretrained_w2v.py

--- pretrained_w2v.py
+++ pretrained_w2v.py
@@ -3,3 +3,10 @@ from future.utils import iteritems
 from builtins import range
 
 # sudo pip install -U future
+
+# pip install gensim
+from gensim.models import KeyedVectors
+
+# A few giga bytes
+word_vectors = KeyedVectors.load_word2vec_format('../large_files/GoogleNews-vectors-negative300.bin', binary=True)
+
